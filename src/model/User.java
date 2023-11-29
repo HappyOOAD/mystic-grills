5b9d6b3b7ca0250c03df98e5102702128ba73b87
jeffjuann <jeffjuann@gmail.com>
2023-11-29 20:47:16 +0700

    feat: complete User model

--- src/model/User.java
+++ src/model/User.java
@@ -1,10 +1,15 @@
 package model;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
+
+import database.Connect;
 
 public class User
 {
-
 	private int userId;
 	private String userRole;
 	private String userName;
@@ -23,33 +28,129 @@ public class User
 	
 	public static void deleteUser(int userId)
 	{
-		
+		String query = "DELETE FROM users WHERE userId = " + userId + ";";
+					  
+		try (Connection connection = Connect.getInstance().getConnection())
+		{
+			Statement statement = connection.createStatement();
+			statement.executeUpdate(query);
+		}
+		catch (SQLException e)
+		{
+			e.printStackTrace();
+		}
 	}
 	
 	
 	public static User getUserById(int userId)
 	{
-		return null;
+		User user = null;
+		String query = "SELECT * FROM users WHERE userId = " + userId + ";";
+		
+		try (Connection connection = Connect.getInstance().getConnection())
+		{
+			Statement statement = connection.createStatement();
+			ResultSet resultSet = statement.executeQuery(query);
+				int id = resultSet.getInt("userId");
+				String role = resultSet.getString("UserRole");
+				String name = resultSet.getString("UserName");
+				String email = resultSet.getString("UserEmail");
+				String password = resultSet.getString("UserPassword");
+				user = new User(id, role, name, email, password);
+		} 
+		catch (SQLException e)
+		{
+			e.printStackTrace();
+		} 
+		return user;
 	}
 	
 	public static void createUser(String userRole, String userName, String userEmail, String userPassword)
 	{
-		
+		String query = "INSERT INTO users(userRole, userName, userEmail, userPassword) VALUES ('Customer','" +
+			userName + "','" +
+			userEmail + "','" +
+			userPassword + "')";
+				  
+		try (Connection connection = Connect.getInstance().getConnection())
+		{
+			Statement statement = connection.createStatement();
+			statement.executeUpdate(query);
+		}
+		catch (SQLException e)
+		{
+			e.printStackTrace();
+		}
 	}
 	
 	public static void updateUser(int userId, String userRole, String userName, String userEmail, String userPassword)
 	{
-		
+		String query = "UPDATE users SET "
+				+ "userRole = " + userRole + ", "
+				+ "userName = " + userName + ", "
+				+ "userEmail = " + userEmail + ", "
+				+ "userPassword = " + userPassword + " "
+				+ "WHERE"
+				+ "userId = " + userId + ";";
+				  
+		try (Connection connection = Connect.getInstance().getConnection())
+		{
+			Statement statement = connection.createStatement();
+			statement.executeUpdate(query);
+		}
+		catch (SQLException e)
+		{
+			e.printStackTrace();
+		}
 	}
 	
 	public static ArrayList<User> getAllUsers()
 	{
-		return null;
+		ArrayList<User> users = new ArrayList<>();
+		String query = "SELECT * FROM users";
+		
+		try (Connection connection = Connect.getInstance().getConnection())
+		{
+			Statement statement = connection.createStatement();
+			ResultSet resultSet = statement.executeQuery(query);
+			while(resultSet.next()) 
+			{
+				int id = resultSet.getInt("userId");
+				String role = resultSet.getString("UserRole");
+				String name = resultSet.getString("UserName");
+				String email = resultSet.getString("UserEmail");
+				String password = resultSet.getString("UserPassword");
+				users.add(new User(id, role, name, email, password));
+			}
+		} 
+		catch (SQLException e)
+		{
+			e.printStackTrace();
+		} 
+		return users;
 	}
 	
-	public boolean authenticateUser(String userName, String userEmail)
+	public User authenticateUser(String userEmail, String userPassword)
 	{
-		return false;
+		User user = null;
+		String query = "SELECT * FROM users WHERE userEmail = " + userEmail + "AND userPassword " + userPassword + ";";
+		
+		try (Connection connection = Connect.getInstance().getConnection())
+		{
+			Statement statement = connection.createStatement();
+			ResultSet resultSet = statement.executeQuery(query);
+				int id = resultSet.getInt("userId");
+				String role = resultSet.getString("UserRole");
+				String name = resultSet.getString("UserName");
+				String email = resultSet.getString("UserEmail");
+				String password = resultSet.getString("UserPassword");
+				user = new User(id, role, name, email, password);
+		} 
+		catch (SQLException e)
+		{
+			e.printStackTrace();
+		} 
+		return user;
 	}
 	
 	
