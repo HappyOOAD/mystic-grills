566e82a979668b266af5566312dad50363e80c7b
jeffjuann <jeffjuann@gmail.com>
2023-11-29 20:25:31 +0700

    feat: add connection

---
+++ src/database/Connect.java
@@ -0,0 +1,47 @@
+package database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Connect {
+    private static Connect instance = null;
+    private Connection connection;
+
+    private Connect() {
+        connectToDatabase();
+    }
+
+    private void connectToDatabase() {
+        try {
+            String url = "jdbc:mysql://localhost:3306/session8";
+            String user = "root";
+            String password = "";
+            this.connection = DriverManager.getConnection(url, user, password);
+        } catch (SQLException e) {
+            throw new RuntimeException("Error connecting to the database", e);
+        }
+    }
+
+    public static Connect getInstance() {
+        if (instance == null) {
+            synchronized (Connect.class) {
+                if (instance == null) {
+                    instance = new Connect();
+                }
+            }
+        }
+        return instance;
+    }
+
+    public Connection getConnection() {
+        try {
+            if (connection == null || connection.isClosed()) {
+                connectToDatabase();
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException("Error checking connection status", e);
+        }
+        return connection;
+    }
+}
--- src/model/MenuItem.java
+++ src/model/MenuItem.java
@@ -1,9 +1,46 @@
 package model;
 
-public class MenuItem {
+import java.util.ArrayList;
 
-	public MenuItem() {
-		// TODO Auto-generated constructor stub
+public class MenuItem
+{
+	private int menuItemId;
+	private String menuItemName;
+	private String menuItemDescription;
+	private int menuItemPrice;
+	
+	public MenuItem(int menuItemId, String menuItemName, String menuItemDescription, int menuItemPrice)
+	{
+		super();
+		this.menuItemId = menuItemId;
+		this.menuItemName = menuItemName;
+		this.menuItemDescription = menuItemDescription;
+		this.menuItemPrice = menuItemPrice;
 	}
-
+	
+	public void createMenuItem(String menuItemName, String menuItemDescription, int menuItemPrice)
+	{
+		
+	}
+	
+	public void updateMenuItem(int menuItemId, String menuItemName, String menuItemDescription, int menuItemPrice)
+	{
+		
+	}
+	
+	public void deleteMenuItem(int menuItemId)
+	{
+		
+	}
+	
+	public MenuItem getMenuItemById(int MenuItemId)
+	{
+		return null;
+	}
+	
+	public ArrayList<MenuItem> getAllMenuItems()
+	{
+		return null;
+	}
+	
 }
