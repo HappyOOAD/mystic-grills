20355da3279cc7e473c86a22afb1e6e1f0a1c59d
jeffjuann <jeffjuann@gmail.com>
2023-11-30 11:17:32 +0700

    fix: SQL Schema

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,48 +1,203 @@
-CREATE DATABASE mysticgrills;
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 30, 2023 at 05:16 AM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.2.0
 
-CREATE TABLE users(
-    userId INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    userRole VARCHAR(20) NOT NULL,
-    userName VARCHAR(30) NOT NULL,
-    userEmail VARCHAR(30) NOT NULL,
-    userPassword VARCHAR(30) NOT NULL  
-);
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
-CREATE TABLE menuitem(
-    menuItemId INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
-    menuItemName VARCHAR(30) NOT NULL,
-    menuItemDescription VARCHAR(30) NOT NULL,
-    menuItemPrice INT NOT NULL 
-);
 
-CREATE TABLE orderitem(
-    orderItemId INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    menutItemId INT NOT NULL,
-    quantity INT NOT NULL,
-    
-    CONSTRAINT fk_menu_item_id FOREIGN KEY (menutItemId) REFERENCES menuitem(menutItemId)
-    
-);
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
-CREATE TABLE orders(
-    orderId INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    orderItemId INT NOT NULL,
-    userId INT NOT NULL,
-    orderStatus VARCHAR(30) NOT NULL,
-    orderDate DATE NOT NULL,
-    orderTotal INT NOT NULL,
-    
-    CONSTRAINT fk_order_item_id FOREIGN KEY (orderItemId) REFERENCES orderitem(orderItemId),
-    CONSTRAINT fk_user_id FOREIGN KEY (userId) REFERENCES users(userId)  
-);
+--
+-- Database: `mysticgrills`
+--
 
-CREATE TABLE receipt(
-    receiptId INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    orderId INT NOT NULL,
-    receiptPaymentAmount INT NOT NULL,
-    receiptPaymentDate DATE NOT NULL,
-    receiptPaymentType VARCHAR(30) NOT NULL,
-    
-    
-    CONSTRAINT fk_order_id FOREIGN KEY (orderId) REFERENCES orders(orderId)  
-);
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `menuitem`
+--
+
+CREATE TABLE `menuitem` (
+  `menuItemId` int(11) NOT NULL,
+  `menuItemName` varchar(30) NOT NULL,
+  `menuItemDescription` varchar(30) NOT NULL,
+  `menuItemPrice` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `menuitem`
+--
+
+INSERT INTO `menuitem` (`menuItemId`, `menuItemName`, `menuItemDescription`, `menuItemPrice`) VALUES
+(1, 'Spageti', 'Welp', 25000);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orderitem`
+--
+
+CREATE TABLE `orderitem` (
+  `orderItemId` int(11) NOT NULL,
+  `orderId` int(11) NOT NULL,
+  `menuItemId` int(11) NOT NULL,
+  `quantity` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orders`
+--
+
+CREATE TABLE `orders` (
+  `orderId` int(11) NOT NULL,
+  `userId` int(11) NOT NULL,
+  `orderStatus` varchar(30) NOT NULL,
+  `orderDate` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `receipt`
+--
+
+CREATE TABLE `receipt` (
+  `receiptId` int(11) NOT NULL,
+  `orderItemId` int(11) NOT NULL,
+  `receiptPaymentAmount` int(11) NOT NULL,
+  `receiptPaymentDate` date NOT NULL,
+  `receiptPaymentType` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `userId` int(11) NOT NULL,
+  `userRole` varchar(20) NOT NULL,
+  `userName` varchar(30) NOT NULL,
+  `userEmail` varchar(30) NOT NULL,
+  `userPassword` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`userId`, `userRole`, `userName`, `userEmail`, `userPassword`) VALUES
+(4, 'Customer', 'Maverick', 'mv@gmail.com', 'mv1234');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `menuitem`
+--
+ALTER TABLE `menuitem`
+  ADD PRIMARY KEY (`menuItemId`);
+
+--
+-- Indexes for table `orderitem`
+--
+ALTER TABLE `orderitem`
+  ADD PRIMARY KEY (`orderItemId`),
+  ADD KEY `fk_menu_item_id` (`menuItemId`),
+  ADD KEY `fk_order_id` (`orderId`);
+
+--
+-- Indexes for table `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`orderId`),
+  ADD KEY `fk_user_id` (`userId`);
+
+--
+-- Indexes for table `receipt`
+--
+ALTER TABLE `receipt`
+  ADD PRIMARY KEY (`receiptId`),
+  ADD KEY `fk_menu_item_ids` (`orderItemId`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`userId`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `menuitem`
+--
+ALTER TABLE `menuitem`
+  MODIFY `menuItemId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `orderitem`
+--
+ALTER TABLE `orderitem`
+  MODIFY `orderItemId` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `orders`
+--
+ALTER TABLE `orders`
+  MODIFY `orderId` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `receipt`
+--
+ALTER TABLE `receipt`
+  MODIFY `receiptId` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `userId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `orderitem`
+--
+ALTER TABLE `orderitem`
+  ADD CONSTRAINT `fk_menu_item_id` FOREIGN KEY (`menuItemId`) REFERENCES `menuitem` (`menuItemId`),
+  ADD CONSTRAINT `fk_order_id` FOREIGN KEY (`orderId`) REFERENCES `orders` (`orderId`);
+
+--
+-- Constraints for table `orders`
+--
+ALTER TABLE `orders`
+  ADD CONSTRAINT `fk_user_id` FOREIGN KEY (`userId`) REFERENCES `users` (`userId`);
+
+--
+-- Constraints for table `receipt`
+--
+ALTER TABLE `receipt`
+  ADD CONSTRAINT `fk_menu_item_ids` FOREIGN KEY (`orderItemId`) REFERENCES `orderitem` (`orderItemId`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
