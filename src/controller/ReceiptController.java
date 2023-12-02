9dc30a09e17955a4783cbcd0e22df4237aead734
jeffjuann <jeffjuann@gmail.com>
2023-12-02 12:49:50 +0700

    feat: receipt controller

--- src/controller/ReceiptController.java
+++ src/controller/ReceiptController.java
@@ -1,9 +1,22 @@
 package controller;
 
-public class ReceiptController {
+import java.util.Date;
 
-	public ReceiptController() {
+import model.Order;
+import model.Receipt;
+
+public class ReceiptController
+{
+
+	public ReceiptController()
+	{
 		// TODO Auto-generated constructor stub
 	}
 
+	public void createReceipt(Order order, String receiptPaymentType, double receiptPaymentAmount, Date receiptPaymentDate)
+	{
+		if(order == null) return;
+		if(receiptPaymentType.isBlank()) return;
+		Receipt.createReceipt(order, receiptPaymentType, receiptPaymentAmount, receiptPaymentDate);
+	}
 }
