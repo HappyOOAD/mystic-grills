3c51e5f8e871902e3968e6afe0d025cecb7f44f5
jeffjuann <jeffjuann@gmail.com>
2023-12-02 13:06:24 +0700

    feat: complete receipt controller

--- src/controller/ReceiptController.java
+++ src/controller/ReceiptController.java
@@ -1,5 +1,6 @@
 package controller;
 
+import java.util.ArrayList;
 import java.util.Date;
 
 import model.Order;
@@ -17,6 +18,35 @@ public class ReceiptController
 	{
 		if(order == null) return;
 		if(receiptPaymentType.isBlank()) return;
+		if(receiptPaymentAmount <= 0.0) return;
+		if(receiptPaymentDate == null) return;
 		Receipt.createReceipt(order, receiptPaymentType, receiptPaymentAmount, receiptPaymentDate);
 	}
+	
+	public void updateReceipt(int orderId, String receiptPaymentType, double receiptPaymentAmount, Date receiptPaymentDate)
+	{
+		if(orderId <= 0) return;
+		if(receiptPaymentType.isBlank()) return;
+		if(receiptPaymentAmount <= 0.0) return;
+		if(receiptPaymentDate == null) return;
+		Receipt.updateReceipt(orderId, receiptPaymentType, receiptPaymentAmount, receiptPaymentDate);
+	}
+	
+	public void deleteReceipt(int orderId)
+	{
+		if(orderId <= 0) return;
+		Receipt.deleteReceipt(orderId);
+	}
+	
+	public Receipt getReceiptById(int receiptId)
+	{
+		return Receipt.getReceiptById(receiptId);
+	}
+	
+	public ArrayList<Receipt> getAllReceipts()
+	{
+		return Receipt.getAllReceipts();
+	}
+	
+	
 }
