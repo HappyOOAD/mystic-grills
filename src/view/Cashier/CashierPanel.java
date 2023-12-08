80773781ec3587cbfcfc147f8577381474d92d48
kevinprtn <90082330+kevinprtn@users.noreply.github.com>
2023-12-08 16:23:56 +0700

    masih mabok

--- src/view/Cashier/CashierPanel.java
+++ src/view/Cashier/CashierPanel.java
@@ -1,6 +1,29 @@
 package view.Cashier;
 
+import java.sql.Date;
+
+import controller.MenuItemController;
+import controller.ReceiptController;
+import javafx.collections.FXCollections;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import model.MenuItems;
+import model.Order;
+import model.Receipt;
 
 public class CashierPanel extends Stage
 {
@@ -14,8 +37,133 @@ public class CashierPanel extends Stage
 		- Can View Order Details
 	*/
 	
+	private BorderPane root;
+    private VBox contentArea;
+    private MenuBar menuBar;
+	
 	public CashierPanel() {
-		// TODO Auto-generated constructor stub
+		 super(StageStyle.DECORATED);
+
+        root = new BorderPane();
+        Scene scene = new Scene(root, 1200, 600);
+        this.setScene(scene);
+
+        menuBar = new MenuBar();
+        Menu Receipts = new Menu("Receipts");
+        MenuItem ReceiptsItem= new MenuItem("Receipt");
+
+        Receipts.getItems().addAll(ReceiptsItem);
+        menuBar.getMenus().addAll(Receipts);
+        
+        Menu Orders = new Menu("Orders");
+        MenuItem OrdersItem = new MenuItem("Show");
+
+        Orders.getItems().addAll(OrdersItem);
+        menuBar.getMenus().addAll(Orders);
+        
+        root.setTop(menuBar);
+        
+        ReceiptsItem.setOnAction(e -> {
+        	Receipts();
+        });
+        OrdersItem.setOnAction(e -> {
+        	Orders();
+        });
+
+        contentArea = new VBox(20);
+        contentArea.setPadding(new Insets(20));
+        contentArea.setAlignment(Pos.CENTER);
+        
+        VBox contentDivide = new VBox(15);
+        contentDivide.setAlignment(Pos.TOP_CENTER);
+        contentDivide.getChildren().addAll( contentArea);
+        root.setCenter(contentDivide);
+	}
+
+	private void Orders() {
+		Label orderIdLabel = new Label("Order ID:");
+        Label totalOrderPriceLabel = new Label("Total Order Price:");
+        Label paymentTypeLabel = new Label("Payment Type:");
+        Label paymentAmountLabel = new Label("Payment Amount:");
+		
+		contentArea.getChildren().clear();
+    	TableView<Order>OrderTable = createOrderTableView();
+		contentArea.getChildren().add(OrderTable);
+		
+		OrderTable.getSelectionModel().selectedItemProperty().addListener((obs, oldSelection, newSelection) -> {
+            if (newSelection != null) {
+            	orderDetail();
+            }
+        });
+	}
+
+	private void orderDetail() {
+		contentArea.getChildren().clear();
+    	
+		
+	}
+
+	private TableView<Order> createOrderTableView() {
+		TableView<Order> tableView = new TableView<>();
+    	tableView.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
+    	
+        TableColumn<Order, Integer> OrderIDColumn = new TableColumn<>("OrderID");
+        OrderIDColumn.setCellValueFactory(new PropertyValueFactory<>("orderId"));
+        OrderIDColumn.setPrefWidth(150);
+        
+        TableColumn<Order, Integer> UserIDColumn = new TableColumn<>("UserID");
+        UserIDColumn.setCellValueFactory(new PropertyValueFactory<>("userId"));
+        UserIDColumn.setPrefWidth(150);
+        
+        TableColumn<Order, String> OrderStatusColumn = new TableColumn<>("Status");
+        OrderStatusColumn.setCellValueFactory(new PropertyValueFactory<>("orderStatus"));
+        OrderStatusColumn.setPrefWidth(150);
+        
+        TableColumn<Order, Date> OrderDateColumn = new TableColumn<>("Date");
+        OrderDateColumn.setCellValueFactory(new PropertyValueFactory<>("orderDate"));
+        OrderDateColumn.setPrefWidth(150);
+        
+        tableView.getColumns().addAll(OrderIDColumn, UserIDColumn, OrderStatusColumn, OrderDateColumn);
+        tableView.setItems(FXCollections.observableArrayList(Order.getAllOrders()));
+        
+		return tableView;
+	}
+
+	private void Receipts() {
+		contentArea.getChildren().clear();
+    	TableView<Receipt>ReceiptsTable = createReceiptsTableView();
+		contentArea.getChildren().add(ReceiptsTable);
+		
+	}
+
+	private TableView<Receipt> createReceiptsTableView() {
+		TableView<Receipt> tableView = new TableView<>();
+    	tableView.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
+    	
+        TableColumn<Receipt, Integer> ReceiptIDColumn = new TableColumn<>("ReceiptID");
+        ReceiptIDColumn.setCellValueFactory(new PropertyValueFactory<>("receiptId"));
+        ReceiptIDColumn.setPrefWidth(150);
+        
+        TableColumn<Receipt, Integer> OrderIDColumn = new TableColumn<>("OrderID");
+        ReceiptIDColumn.setCellValueFactory(new PropertyValueFactory<>("orderId"));
+        ReceiptIDColumn.setPrefWidth(150);
+        
+        TableColumn<Receipt, Double> ReceiptAmountColumn = new TableColumn<>("Amount");
+        ReceiptIDColumn.setCellValueFactory(new PropertyValueFactory<>("receiptPaymentAmount"));
+        ReceiptIDColumn.setPrefWidth(150);
+        
+        TableColumn<Receipt, Date> ReceiptDateColumn = new TableColumn<>("Date");
+        ReceiptIDColumn.setCellValueFactory(new PropertyValueFactory<>("receiptPaymentDate"));
+        ReceiptIDColumn.setPrefWidth(150);
+        
+        TableColumn<Receipt, String> ReceiptPaymentTypeColumn = new TableColumn<>("Payment Type");
+        ReceiptIDColumn.setCellValueFactory(new PropertyValueFactory<>("receiptPaymentType"));
+        ReceiptIDColumn.setPrefWidth(150);
+        
+        tableView.getColumns().addAll(ReceiptIDColumn, OrderIDColumn, ReceiptAmountColumn, ReceiptDateColumn, ReceiptPaymentTypeColumn);
+        tableView.setItems(FXCollections.observableArrayList(Receipt.getAllReceipts()));
+        
+		return tableView;
 	}
 
 }
--- src/view/Customer/CustomerPanel.java
+++ src/view/Customer/CustomerPanel.java
@@ -1,14 +1,172 @@
 package view.Customer;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import controller.MenuItemController;
+import controller.OrderController;
+import javafx.collections.FXCollections;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import model.MenuItems;
+import model.Order;
+import model.OrderItem;
+import model.User;
 
 public class CustomerPanel extends Stage
 {
+	private BorderPane root;
+    private VBox contentArea;
+    private MenuBar menuBar;
+    
+    private Stage stage;
+    private ArrayList<MenuItem> menuItems;
+    private Order currentOrder;
 
-	public CustomerPanel()
-	{
-		// TODO Auto-generated constructor stub
+	public CustomerPanel(){
+		this.menuItems = menuItems;
+
+		menuBar = new MenuBar();
+        Menu addMenu = new Menu("Add");
+        MenuItem addMenuItem= new MenuItem("Show");
+        
+        addMenu.getItems().addAll(addMenuItem);
+        menuBar.getMenus().addAll(addMenu);
+        
+        addMenuItem.setOnAction(e -> createMenuItemsPane());    
+        
+        Menu updateMenu = new Menu("Update");
+        MenuItem updateMenuItem= new MenuItem("Show");
+        
+        updateMenu.getItems().addAll(updateMenuItem);
+        menuBar.getMenus().addAll(updateMenu);
+        
+        addMenuItem.setOnAction(e -> createMenuItemsPane());
+        updateMenuItem.setOnAction(e -> createOrderedMenuItemsPane()); 
+        
+        root = new BorderPane();
+        Scene scene = new Scene(root, 1200, 600);
+        this.setScene(scene);
+
+        contentArea = new VBox(20);
+        contentArea.setPadding(new Insets(20));
+        contentArea.setAlignment(Pos.CENTER);
+        
+        VBox contentDivide = new VBox(15);
+        contentDivide.setAlignment(Pos.TOP_CENTER);
+        contentDivide.getChildren().addAll( contentArea);
+        root.setCenter(contentDivide);
 		
 	}
+
+	private Object createOrderedMenuItemsPane() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private GridPane createMenuItemsPane() {
+		contentArea.getChildren().clear();
+    	TableView<MenuItems>menuItemTable = createMenuItemTableView();
+		contentArea.getChildren().add(menuItemTable);
+		AtomicInteger idSelectedMenu = new AtomicInteger();
+		MenuItems menu;
+		User user;
+		
+		menuItemTable.getSelectionModel().selectedItemProperty().addListener((obs, oldSelection, newSelection) -> {
+            if (newSelection != null) {
+            	idSelectedMenu.set(newSelection.getMenuItemId());
+            }
+        });
+		int selectedMenuId = idSelectedMenu.get();
+		menu = MenuItems.getMenuItemById(selectedMenuId);
+		
+		GridPane gridPane = new GridPane();
+        gridPane.setPadding(new Insets(20));
+        gridPane.setHgap(10);
+        gridPane.setVgap(10);
+
+        Label quantityLabel = new Label("Quantity:");
+        TextField quantityTextField = new TextField();
+
+        Button addToOrderButton = new Button("Add to Order");
+        addToOrderButton.setOnAction(event -> addToOrder(menu, quantityTextField.getText()));
+
+        Button submitOrderButton = new Button("Submit Order");
+        submitOrderButton.setOnAction(event -> submitOrder());
+
+        gridPane.add(quantityLabel, 0, 1);
+        gridPane.add(quantityTextField, 1, 1);
+        gridPane.add(addToOrderButton, 0, 2);
+        gridPane.add(submitOrderButton, 1, 2);
+
+        return gridPane;
+	}
+	
+	private void addToOrder(MenuItems menuItem, String quantityStr) {
+        try {
+            int quantity = Integer.parseInt(quantityStr);
+
+            // Validation checks
+            if (menuItem != null && quantity > 0) {
+//            	menuItems.add() masukkin menu yang dipilih nya ke menuItems 
+            } else {
+                System.out.println("Invalid input. Please check your input values.");
+            }
+        } catch (NumberFormatException e) {
+            System.out.println("Invalid quantity. Please enter a valid number.");
+        }
+    }
+	
+	private void submitOrder() {
+		//
+		Date currentDate = new Date();
+		// kenapa order item yang di masukkin ke arraylist?
+		Order.createOrder(1, menuItems, currentDate);
+    }
+
+	private TableView<MenuItems> createMenuItemTableView() {
+    	TableView<MenuItems> tableView = new TableView<>();
+    	tableView.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
+    	
+        TableColumn<MenuItems, Integer> menuItemIdColumn = new TableColumn<>("ID");
+        menuItemIdColumn.setCellValueFactory(new PropertyValueFactory<>("menuItemId"));
+        menuItemIdColumn.setPrefWidth(150);
+    	    
+        TableColumn<MenuItems, String> menuItemNameColumn = new TableColumn<>("Name");
+        menuItemNameColumn.setCellValueFactory(new PropertyValueFactory<>("menuItemName"));
+        menuItemNameColumn.setPrefWidth(150);
+        
+        TableColumn<MenuItems, String> menuItemDescriptionColumn = new TableColumn<>("Description");
+        menuItemDescriptionColumn.setCellValueFactory(new PropertyValueFactory<>("menuItemDescription"));
+        menuItemDescriptionColumn.setPrefWidth(150);
+        
+        TableColumn<MenuItems, Integer> menuItemPriceColumn = new TableColumn<>("Price");
+        menuItemPriceColumn.setCellValueFactory(new PropertyValueFactory<>("menuItemPrice"));
+        menuItemPriceColumn.setPrefWidth(150);       
+        
+        tableView.getColumns().addAll(menuItemIdColumn, menuItemNameColumn, menuItemDescriptionColumn, menuItemPriceColumn);
+        tableView.setItems(FXCollections.observableArrayList(MenuItemController.getAllMenuItem()));
+        
+        return tableView;
+    }
 
 }
