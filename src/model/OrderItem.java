04b75ebf5a258fc97ccfb8f1aa1c1d23129d4783
jeffjuann <jeffjuann@gmail.com>
2023-12-19 15:21:30 +0700

    fix: logic at model OrderItem

--- src/model/OrderItem.java
+++ src/model/OrderItem.java
@@ -122,7 +122,7 @@ public class OrderItem
 		{
 			PreparedStatement prep = connection.prepareStatement(query);
 			prep.setInt(1, orderId);
-			prep.setInt(1, menuItemId);
+			prep.setInt(2, menuItemId);
 			ResultSet resultSet = prep.executeQuery();
 			
 			if(resultSet.next()) 
