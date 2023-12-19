09d82661338bf31c2fda3eee114827157344859f
ReynardChristiansen <reynard.satria@gmail.com>
2023-12-19 15:13:36 +0700

    update waiter panel like ChefPanel

--- src/view/Chef/ChefPanel.java
+++ src/view/Chef/ChefPanel.java
@@ -57,7 +57,7 @@ public class ChefPanel extends Stage implements IAddOrderItemParentPanel
 		super(StageStyle.DECORATED);
 		setTitle("Mystic Grills - Chef Panel");
         root = new BorderPane();
-        Scene scene = new Scene(root, 1280, 720);
+        Scene scene = new Scene(root, 1580, 900);
         setScene(scene);
         
         // MENUBARS
